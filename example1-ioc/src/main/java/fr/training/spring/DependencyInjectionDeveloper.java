a7df3ab03a3aebba1b5bf7668ef649590c75d8c1
bnasslah <badr.nasslahsen@gmail.com>
2021-10-11 15:25:10 +0200

    init

--- example1-ioc/src/main/java/fr/training/spring/DependencyInjectionDeveloper.java
+++ example1-ioc/src/main/java/fr/training/spring/DependencyInjectionDeveloper.java
@@ -1,12 +1,15 @@
 package fr.training.spring;
 
 
+import org.springframework.stereotype.Component;
+
+@Component("springDeveloper")
 public class DependencyInjectionDeveloper {
-	
+
 	private Task task;
-	
-	public DependencyInjectionDeveloper(Task task) {
-		this.task = task;
+
+	public DependencyInjectionDeveloper(Task documentingTask) {
+		this.task = documentingTask;
 	}
 	
 	public void doTask() {
--- example1-ioc/src/main/java/fr/training/spring/Developer.java
+++ example1-ioc/src/main/java/fr/training/spring/Developer.java
@@ -1,7 +1,14 @@
 package fr.training.spring;
 
-public class Developer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Developer{
+
+	@Autowired
+	@Qualifier("documentingTask")
 	private Task task;
 	
 	public Developer() {
