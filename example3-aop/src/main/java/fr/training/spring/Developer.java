c12575a5cc8aa739a2006bbbc7c8b3fed53f8543
bnasslah <badr.nasslahsen@gmail.com>
2021-10-11 17:23:38 +0200

    sol 3

---
+++ example3-aop/src/main/java/fr/training/spring/Developer.java
@@ -0,0 +1,18 @@
+package fr.training.spring;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Developer {
+	 
+	private static final Log LOGGER = LogFactory.getLog(Developer.class);
+	
+	public void doTask(Task task) {
+		task.execute();
+	}
+	
+	public void takeABreak(long milliseconds) throws InterruptedException {
+		Thread.sleep(milliseconds);
+	}
+	
+}
