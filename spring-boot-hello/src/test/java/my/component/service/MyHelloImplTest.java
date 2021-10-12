544a514cfa943dd65c5a56c4762393c9a3d68469
bnasslah <badr.nasslahsen@gmail.com>
2021-10-12 14:10:29 +0200

    sol properties

---
+++ spring-boot-hello/src/test/java/my/component/service/MyHelloImplTest.java
@@ -0,0 +1,24 @@
+package my.component.service;
+
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest(classes = AppConfig.class)
+public class MyHelloImplTest {
+
+	@Autowired
+	MyHello myHello;
+
+	@Test
+	public void testSayHello() {
+		Assert.notNull(myHello);
+		String result = myHello.sayHello("Toto");
+		org.junit.Assert.assertEquals("Hello : Toto", result);
+	}
+}
---
+++ spring-boot-starter/src/main/java/my/component/service/starter/AppConfig.java
@@ -0,0 +1,13 @@
+package my.component.service.starter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	StarterHello myHello(){
+		return new StarterHelloImpl();
+	}
+}
