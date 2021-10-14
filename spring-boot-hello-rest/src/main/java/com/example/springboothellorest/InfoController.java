b057bf8df8eca7804958fa9d9d5a1babed58b7e2
bnasslah <badr.nasslahsen@gmail.com>
2021-10-14 10:02:25 +0200

    sol shop-app

--- spring-boot-hello-rest/src/main/java/com/example/springboothellorest/InfoController.java
+++ spring-boot-hello-rest/src/main/java/com/example/springboothellorest/InfoController.java
@@ -1,9 +1,12 @@
 package com.example.springboothellorest;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,16 @@ public class InfoController {
 	@GetMapping
 	public List<Info> findAll() {
 		return infoRepository.findAll();
+	}
+
+	@GetMapping("/{idInfo}")
+	public ResponseEntity<Info> findOneRepo(@PathVariable Long idInfo) {
+		try {
+			Info info = infoRepository.findById(idInfo).get();
+			return ResponseEntity.ok(info);
+		}
+		catch (NoSuchElementException e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
