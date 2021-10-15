ee53e4656474338817f6dfb04e3ff573acb043cd
bnasslah <badr.nasslahsen@gmail.com>
2021-10-15 16:30:47 +0200

    project webclient support

--- shop-app/shop-app-webmvc/src/main/java/fr/training/samples/spring/shop/config/MvcConfiguration.java
+++ shop-app/shop-app-webmvc/src/main/java/fr/training/samples/spring/shop/config/MvcConfiguration.java
@@ -1,16 +1,28 @@
 package fr.training.samples.spring.shop.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class MvcConfiguration {
 
+	@Value("${spring.shop.api.url}")
+	private String apiUrl;
+
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
 		return restTemplateBuilder.build();
+	}
+
+	@Bean
+	public WebClient webClient(){
+		return WebClient.builder()
+				.baseUrl(apiUrl)
+				.build();
 	}
 
 }
--- shop-app/shop-app-webmvc/src/main/java/fr/training/samples/spring/shop/controller/order/OrderController.java
+++ shop-app/shop-app-webmvc/src/main/java/fr/training/samples/spring/shop/controller/order/OrderController.java
@@ -4,27 +4,26 @@ package fr.training.samples.spring.shop.controller.order;
  * @author bnasslahsen
  */
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import fr.training.samples.spring.shop.controller.item.ItemDTO;
+import reactor.core.publisher.Mono;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
 public class OrderController {
-
-
-	@Autowired
-	private RestTemplate restTemplate;
 
 	@Value("${spring.shop.showItems.url}")
 	private String showItemsUrl;
@@ -32,12 +31,20 @@ public class OrderController {
 	@Value("${spring.shop.addOrder.url}")
 	private String addOrderUrl;
 
+	@Autowired
+	private WebClient webClient;
 
 	@GetMapping("/addOrder")
 	public String showOrders(Model model) {
 		OrderDTO orderDTO = new OrderDTO();
-		ItemDTO[] itemDTOS = restTemplate.getForObject(showItemsUrl, ItemDTO[].class);
-		orderDTO.setItems( Arrays.stream(itemDTOS).collect(Collectors.toSet()));
+		Set<ItemDTO> itemDTOS = webClient.get()
+				.uri("/api/items")
+				.retrieve()
+				.bodyToMono(new ParameterizedTypeReference<Set<ItemDTO>>() {})
+				.block();
+
+		orderDTO.setItems(itemDTOS);
+		orderDTO.setItemIDs(new HashSet<>());
 		model.addAttribute("orderModel", orderDTO);
 		return "addOrder";
 	}
